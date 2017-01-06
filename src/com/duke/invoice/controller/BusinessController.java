c819fbc772b624715be4716ca2b6e89388a0678e
root <root@parthiv-HP-EliteBook-8440p>
2017-01-05 21:07:08 -0500

    adding few restfull services..

--- src/com/duke/invoice/controller/BusinessController.java
+++ src/com/duke/invoice/controller/BusinessController.java
@@ -1,6 +1,14 @@
 package com.duke.invoice.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.duke.inovice.domain.Business;
+import com.duke.inovice.domain.Login;
 
 /**
  * Main controller for all business related activity.
@@ -17,13 +25,39 @@ public class BusinessController {
 	 * @return
 	 * @throws Exception
 	 */
-	public Response doLogin(String username, String password) throws Exception {
-		return null;
+	
+	@RequestMapping(value = "/login" , method = RequestMethod.POST)
+	public @ResponseBody Response doLogin(@RequestParam("username") String username,
+			@RequestParam("password") String password) {
+        Response response = new Response();
+        if (username.equals("test") && password.equals("test")){
+        	response.setResult("Success");
+        	response.setCode("001");
+        }else{
+        	response.setResult("Failed");
+        	response.setCode("002");
+        }
+	   //Person person=personService.savedata(jsonString);
+	   return response;
 	}
 	
-	public Response editBusinessDetail() throws Exception {
-		return null;
+	@RequestMapping(value = "/register" , method = RequestMethod.POST)
+	public @ResponseBody Response doLogin(@RequestParam("username") String username , @RequestParam("password") String password ,
+			@RequestParam("industry") String industry) {
+    
+            Response response = new Response();
+        	response.setResult("Success");
+        	response.setCode("001");
+       
+	   return response;
 	}
 	
+	@RequestMapping(value = "/editCompanyDetail" , method = RequestMethod.POST)
+	public Response editBusinessDetail(@RequestBody Business business) throws Exception {
+		Response response = new Response();
+       	response.setResult("Success");
+     	response.setCode("001");
+     	return response;
+    }	
 	
 }
